0e88cd78fd081cdb003963eeaf9e80f690061dd6
satyanarayana-zemoso <satyanarayana.senapathi@zemosolabs.com>
2023-02-07 22:30:26 +0530

    fix: fix in style and remove comments

--- js_assignment/Assignment - ES6/ES6-assignment.js
+++ js_assignment/Assignment - ES6/ES6-assignment.js
@@ -1,39 +1,21 @@
-// Refactor the following function into a one-liner:
-// const printName = (name) => {
-//                      return “Hi” + name;
-//            }
+
 
 const printName = name => "Hi " + name;
 
 console.log(printName("satyanarayana"));
 
-// Rewrite the following code using template literals
-// const printBill = (name, bill) => {
-//                      return “Hi “ + name + “, please pay: “ + bill;
-//            }
 
 const printBill = (name, bill) => `Hi ${name}, please pay: ${bill}`;
 
 console.log(printBill("satyanarayana", 1000));
 
 
-// Modify the following code such that the object properties are destructured and logged.
-// const person = {
-//                       name: “Noam Chomsky”,
-//                       age: 92
-//             }
-
-//            let name = person.name;
-//            let age = person.age;
-//            console.log(name);
-//            console.log(age);
-
 
 const person = {
-    name: "Noam Chomsky",
+    names: "Noam Chomsky",
     age: 92
 };
 
-let { name, age } = person;
-console.log(name);
+let { names, age } = person;
+console.log(names);
 console.log(age);
--- js_assignment/JS Assignment 1 (Functions)/functions.js
+++ js_assignment/JS Assignment 1 (Functions)/functions.js
@@ -1,4 +1,3 @@
-//1  Write a program to demonstrate how a function can be passed as a parameter to another function.
 function greet() {
     return 'Hello';
 }
@@ -12,12 +11,8 @@ name('srinivas', greet);
 name('akash', greet);
 
 
-
-//2  An arrow function takes two arguments firstName and lastName and returns a 2 letter string that represents
-// the first letter of both the arguments. For the arguments Roger and Waters, the function returns ‘RW’. Write this function.
-//Submit the github link to the code
-var prompt = require('prompt-sync')();
-var firstname = prompt('Enter First Name ');
-var secondname = prompt('Enter second Name ');
-var first_letters=(firstname,secondname) => firstname[0]+secondname[0];
+let prompt = require('prompt-sync')();
+let firstname = prompt('Enter First Name ');
+let secondname = prompt('Enter second Name ');
+let first_letters=(firstname,secondname) => firstname[0]+secondname[0];
 console.log(first_letters(firstname,secondname));
